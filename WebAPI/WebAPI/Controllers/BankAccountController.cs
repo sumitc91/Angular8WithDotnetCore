200c80d2d5f34c8d976a96774ee130e5256b50b4
Sumit Chourasia <sumitchourasia91@gmail.com>
2019-11-07 14:07:11 +0530

    Angular 8 Dotnet core initial commit

---
+++ WebAPI/WebAPI/Controllers/BankAccountController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAccountController : ControllerBase
+    {
+        private readonly APIDBContext _context;
+
+        public BankAccountController(APIDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BankAccount
+        [HttpGet]
+        public IEnumerable<BankAccount> GetBankAccounts()
+        {
+            return _context.BankAccounts;
+        }
+
+        // GET: api/BankAccount/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBankAccount([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
+
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bankAccount);
+        }
+
+        // PUT: api/BankAccount/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBankAccount([FromRoute] int id, [FromBody] BankAccount bankAccount)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bankAccount.BankAccountID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bankAccount).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BankAccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BankAccount
+        [HttpPost]
+        public async Task<IActionResult> PostBankAccount([FromBody] BankAccount bankAccount)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BankAccounts.Add(bankAccount);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBankAccount", new { id = bankAccount.BankAccountID }, bankAccount);
+        }
+
+        // DELETE: api/BankAccount/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBankAccount([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            _context.BankAccounts.Remove(bankAccount);
+            await _context.SaveChangesAsync();
+
+            return Ok(bankAccount);
+        }
+
+        private bool BankAccountExists(int id)
+        {
+            return _context.BankAccounts.Any(e => e.BankAccountID == id);
+        }
+    }
+}
