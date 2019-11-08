b4899cd41026216656c406e1146efca8bfd69455
Sumit Chourasia <sumitchourasia91@gmail.com>
2019-11-08 12:50:41 +0530

    Add Update delete row

--- AngularInlineTableCRUD/src/app/bank-account/bank-account.component.ts
+++ AngularInlineTableCRUD/src/app/bank-account/bank-account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray } from '@angular/forms';
+import { FormBuilder, FormArray, Validators, FormGroup } from '@angular/forms';
 import { BankService } from '../shared/bank.service';
+import { BankAccountService } from '../shared/bank-account.service';
 
 @Component({
   selector: 'app-bank-account',
@@ -12,21 +13,69 @@ export class BankAccountComponent implements OnInit {
   bankAccountForms : FormArray = this.fb.array([]);
   bankList = [];
 
-  constructor(private fb: FormBuilder, private bankService: BankService) { }
+  constructor(private fb: FormBuilder, 
+    private bankService: BankService,
+    private service: BankAccountService
+    ) { }
 
   ngOnInit() {
     this.bankService.getBankList()
     .subscribe(res => this.bankList = res as []);
-    this.addBankAccountForm()
+    
+
+    this.service.getBankAccountList().subscribe(
+      res => {
+        if(res ==[]){
+          this.addBankAccountForm()
+        }
+        else{
+          (res as []).forEach((bankAccount : any) => {
+            this.bankAccountForms.push(this.fb.group({
+              bankAccountID : bankAccount.bankAccountID,
+              accountNumber : [bankAccount.accountNumber, Validators.required],
+              accountHolder : [bankAccount.accountHolder, Validators.required],
+              bankID : [bankAccount.bankID, Validators.min(1)],
+              IFSC : [bankAccount.ifsc, Validators.required]
+            }));
+          });
+        }
+      }
+    )
   }
 
   addBankAccountForm(){
     this.bankAccountForms.push(this.fb.group({
       bankAccountID : [0],
-      accountNumber : [''],
-      accountHolder : [''],
-      bankID : [0],
-      IFSC : ['']
+      accountNumber : ['', Validators.required],
+      accountHolder : ['', Validators.required],
+      bankID : [0, Validators.min(1)],
+      IFSC : ['', Validators.required]
     }));
+  }
+
+  recordSubmit(fg:FormGroup){
+    if(fg.value.bankAccountID == 0){
+      this.service.postBankAccount(fg.value)
+      .subscribe(
+        (res:any) => {
+          fg.patchValue({ bankAccountID: res.bankAccountID});
+        }
+      )
+    }
+    else{
+      this.service.putBankAccount(fg.value)
+      .subscribe(
+        (res:any) => {
+          
+        }
+      )
+    }    
+  }
+
+  onDelete(bankAccountID, i){
+    this.service.deleteBankAccount(bankAccountID).subscribe(
+      res => {
+        this.bankAccountForms.removeAt(i);
+      });
   }
 }
--- AngularInlineTableCRUD/src/app/shared/bank-account.service.ts
+++ AngularInlineTableCRUD/src/app/shared/bank-account.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankAccountService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postBankAccount(formData){
+    return this.http.post(environment.apiBaseURI+'/BankAccount', formData)
+  }
+
+  putBankAccount(formData){
+    return this.http.put(environment.apiBaseURI+'/BankAccount/'+formData.bankAccountID, formData)
+  }
+
+  getBankAccountList(){
+    return this.http.get(environment.apiBaseURI+'/BankAccount');
+  }
+
+  deleteBankAccount(id){
+    return this.http.delete(environment.apiBaseURI+'/BankAccount/'+id)
+  }
 }
